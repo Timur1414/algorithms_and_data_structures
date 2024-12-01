22ab677658bc41fde8d129d2837f1fd0375731a0
Timur <tima.temirov.2005@mail.ru>
2024-12-01 17:42:11 +0300

    Added dfs.

---
+++ graph/dfs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int graph [100][100]; // матрица смежности
+int n; // количество вершин
+bool visited[100]; // изначально все false
+
+void dfs (int v) { // обход в глубину
+    visited[v] = true;
+    for (int i = 0; i < n; i++)
+        if (graph[v][i] && !visited[i])
+            dfs(i);
+}
+
+int main() {
+
+
+    return 0;
+}
