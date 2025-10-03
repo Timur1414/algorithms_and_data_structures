19d87868ccdd07b46796fd4fb38fe77ac94155b9
Timur <tima.temirov.2005@mail.ru>
2025-10-03 20:22:59 +0300

    Added fast power.

---
+++ math/power.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+double power(double a, int n) {
+    double res = 1;
+    double a_in_power2 = a;
+    while (n > 0) {
+        if (n & 1 == 1)
+            res *= a_in_power2;
+        a_in_power2 *= a_in_power2;
+        n = n >> 1;  // если n отрицательное, то цикл будет бесконечным, так как знаковый бит не участвует в битовом сдвиге.
+    }
+    return res;
+}
+
+int main() {
+    double a = 2;
+    int n = 8;
+    cout << power(a, n);
+
+
+    return 0;
+}
